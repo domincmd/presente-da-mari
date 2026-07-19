72bb46d00e9b86ab005f804dfc078f41b52cc9e1
domincmd <felipe.caliento.domingueti@gmail.com>
2026-07-19 18:15:03 -0300

    added sum more thingys

---
+++ public/scripts/chorme.js
@@ -0,0 +1,44 @@
+const pages = {
+    "qu3c0m3cem0sjo6os.com": "1.html",
+};
+
+const iframe = document.querySelector(".iframe");
+const urlInput = document.querySelector(".url-input");
+
+const history = [];
+let page = -1;
+
+function loadPage(input, addToHistory = true) {
+    const file = pages[input] ?? "notfound.html";
+
+    iframe.src = `pages/${file}`;
+    urlInput.value = input;
+
+    if (addToHistory && file !== "notfound.html") {
+        // Remove forward history if we're not at the end
+        history.splice(page + 1);
+
+        history.push(input);
+        page = history.length - 1;
+    }
+}
+
+urlInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        loadPage(urlInput.value);
+    }
+});
+
+function back() {
+    if (page <= 0) return;
+
+    page--;
+    loadPage(history[page], false);
+}
+
+function front() {
+    if (page >= history.length - 1) return;
+
+    page++;
+    loadPage(history[page], false);
+}
